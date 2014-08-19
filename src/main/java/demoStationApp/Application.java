640cccda935d748b851c26b7ad849715ab1c6bb6
Wolfgang Kluth <me@kluth.co.uk>
2014-08-19 12:03:36 +0200

    change language level to 1.7 and removed h2 drivers

--- src/main/java/demoStationApp/Application.java
+++ src/main/java/demoStationApp/Application.java
@@ -5,12 +5,9 @@ import demoStationApp.domain.Pedelec;
 import demoStationApp.domain.Slot;
 import demoStationApp.domain.Station;
 import demoStationApp.repository.StationRepository;
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
